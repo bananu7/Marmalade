b0772075180ec4fe9bcd2d43f4458f5451c3cbff
Bartek Banachewicz <bananu7@o2.pl>
2015-03-22 19:39:29 +0100

    Removed some redundant stuff

--- Marmalade/app.ts
+++ Marmalade/app.ts
@@ -1,28 +1,16 @@
-﻿class Greeter {
-    element: HTMLElement;
-    span: HTMLElement;
-    timerToken: number;
+﻿ ///<reference path="lib/jquery.d.ts" />
 
-    constructor(element: HTMLElement) {
-        this.element = element;
-        this.element.innerHTML += "The time is: ";
-        this.span = document.createElement('span');
-        this.element.appendChild(this.span);
-        this.span.innerText = new Date().toUTCString();
+// Find the right method, call on correct element
+function launchFullScreen(element) {
+    if (element.requestFullScreen) {
+        element.requestFullScreen();
+    } else if (element.mozRequestFullScreen) {
+        element.mozRequestFullScreen();
+    } else if (element.webkitRequestFullScreen) {
+        element.webkitRequestFullScreen();
     }
-
-    start() {
-        this.timerToken = setInterval(() => this.span.innerHTML = new Date().toUTCString(), 500);
-    }
-
-    stop() {
-        clearTimeout(this.timerToken);
-    }
-
 }
 
 window.onload = () => {
-    var el = document.getElementById('content');
-    var greeter = new Greeter(el);
-    greeter.start();
+    document.getElementById("fullscreenButton").addEventListener('click',() => launchFullScreen(document.documentElement));
 };
